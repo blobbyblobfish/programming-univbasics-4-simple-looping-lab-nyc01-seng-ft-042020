ddf3d1f65a19b21a729e47ccea3f09cbb08e74d2
Alice Wang <aywang6@gmail.com>
2020-03-31 16:03:10 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -15,3 +15,21 @@ def loop_message_n_times (string, n)
   index += 1 
 end
 end
+
+def output_array (array)
+  index = 0 
+  while index < array.length do
+    puts array[index]
+    index += 1 
+  end
+end
+
+def return_string_array (array)
+  index = 0 
+  new_array= 0
+  while index < array.length do
+    new_array.push(array[index].to_s)
+    index += 1 
+  end
+  new_array
+end
